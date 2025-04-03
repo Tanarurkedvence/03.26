d48f4a3c43347e7f936ea529b412182357a6396f
Neon <urban.marcell44@gmail.com>
2025-04-03 12:13:23 +0200

    asd

---
+++ Assets/scripts/flybehaivor.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class flybehaivor : MonoBehaviour
+{
+    [SerializeField]
+    private float _velocity = 1.5f;
+
+    [SerializeField]
+    private float _rotationSpeed = 10f;
+
+    private Rigidbody2D _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if(Mouse.current.leftButton.wasPressedThisFrame) 
+        {
+            _rigidbody.linearVelocity = Vector2.up * _velocity;
+        }
+    }
+    private void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, _rigidbody.linearVelocityY * _rotationSpeed);
+
+    }
+}
